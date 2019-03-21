9742ac7fff4760525222d07ea81a1d0b05b1860b
Ruslan <rasel579@gmail.com>
2019-03-21 23:11:27 +0500

    added access via JWT using furebase auth

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import firebase from 'firebase';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUpForm from './components/SignUpFrom';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyAx6ofolJMeXLUiYH44cOHil76Kt7eC-I4",
+      authDomain: "one-time-password-ad510.firebaseapp.com",
+      databaseURL: "https://one-time-password-ad510.firebaseio.com",
+      projectId: "one-time-password-ad510",
+      storageBucket: "one-time-password-ad510.appspot.com",
+      messagingSenderId: "111745114235"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <View>
--- components/SignInForm.js
+++ components/SignInForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import  axios  from 'axios';
+import firebase from 'firebase';
 
 const ROOT_URL = 'https://us-central1-one-time-password-ad510.cloudfunctions.net'
 
@@ -11,10 +12,13 @@ class SignInForm extends Component {
 
     handleSubmitCode = async () => {
         try{
-            let response = await axios.post(`${ROOT_URL}/verifyOneTimePassword`, {
+            let { data } = await axios.post(`${ROOT_URL}/verifyOneTimePassword`, {
                 phone: this.state.phone,
                 code: this.state.code
                 });
+            
+                firebase.auth().signInWithCustomToken(data.token);
+
         } catch(err){
               console.log(err);
         };
