fba009bacb565e8be6f46383473749d99602a89f
Ruslan <rasel579@gmail.com>
2019-03-21 17:56:03 +0500

    finished the SignUpForm with await/async functions and google functions

--- components/SignUpFrom.js
+++ components/SignUpFrom.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Input, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-ad510.cloudfunctions.net'
 
 class SignUpForm extends Component{    
-    state = { phone : '' }; //es2017
+    state = { phone: '' }; //es2017
    
-    handleSubmit = () => { //es2017 that's why we are not using bind(this) on eventHandler
-       
+    handleSubmit = async () => { //es2017 that's why we are not using bind(this) on eventHandler
+    // async/await feature from es2017
+    try {
+       await axios.post( `${ROOT_URL}/createUser`, { phone: this.state.phone })
+       await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone })
+    }   catch(err){
+        console.log(err.response.data);
+    }
+         
     }
 
+    // handleSubmit = () => { //es2017 that's why we are not using bind(this) on eventHandler
+    // // async/await feature from es2017
+    //    axios.post( `${ROOT_URL}/createUser`, {
+    //        phone: this.state.phone
+    //    })
+    //      .then(() => {
+    //          axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+    //              phone: this.state.phone
+    //          })
+    //      })
+         
+    // }
+
     render(){
+        console.log(this.state);
       return (
           <View style={{ marginTop: 100, marginLeft: 20, marginRight: 20 }}>
               <View style={{ marginBottom: 20 }}>
